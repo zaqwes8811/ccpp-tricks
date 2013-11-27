7420d5261f8724ae0270c0eb808d99b604bce383
zaqwes8811 <igor.a.lugansky@gmail.com>
2013-11-27 22:40:09 +0400

    Wrap point and change from js

--- embedded_script_engines/v8/src/process.h
+++ embedded_script_engines/v8/src/process.h
@@ -160,3 +160,5 @@ void LogCallback(const v8::FunctionCallbackInfo<v8::Value>& args);
 // much with string encodings we just use ascii.
 string ObjectToString(Local<Value> value);
 
+bool ExecuteScript(Handle<String> script, Isolate* isolate);
+
