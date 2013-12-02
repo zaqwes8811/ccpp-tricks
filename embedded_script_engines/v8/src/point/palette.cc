b52db6a364009504eb70e48d01d1ba92c627d44f
zaqwes <igor.a.lugansky@gmail.com>
2013-12-02 11:24:06 +0300

    Haaard

--- embedded_script_engines/v8/src/point/palette.cc
+++ embedded_script_engines/v8/src/point/palette.cc
@@ -1,2 +1,4 @@
+#include "palette.h"
 
+v8::Persistent<v8::ObjectTemplate> V8Palette::point_blueprint_;
 
--- embedded_script_engines/v8/src/point/palette.h
+++ embedded_script_engines/v8/src/point/palette.h
@@ -1,5 +1,9 @@
 #pragma once
 
+// Other
+#include "v8.h"
+
+// App
 #include "point.h"
 
 class Palette {
@@ -11,6 +15,14 @@ class Palette {
 
 class V8Palette {
 
+
+private:
+
+  // Все что нужно для работы с точкой.
+  v8::Persistent<v8::ObjectTemplate> point_blueprint_;
+
+  // Все что нужно для работы с массивом точек.
+  v8::Persistent<v8::ObjectTemplate> point_array_blueprint_;
 };
 
 class PaletteEngine {
