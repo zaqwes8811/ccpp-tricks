d0594bc9a11266151ef55fbd8087eab4edfaa4e1
zaqwes <igor.a.lugansky@gmail.com>
2013-12-06 11:29:34 +0300

    Begin make source file.

--- embedded_script_engines/tools/point_gen.py
+++ embedded_script_engines/tools/point_gen.py
@@ -9,7 +9,8 @@ from generator.cpp import utils
 
 # App
 from _units import extract_variable_declaration
-from _units import make_wrapper_class
+from _units import make_header
+from _units import make_source
 
 
 def main():
@@ -21,34 +22,29 @@ def main():
 
     #if
     # Make V8 view
-    declarations = []
     impls = []
+    declarations = []
     for elem in vars:
-        d, i = elem.make_scalar_getter()
-        if i:
-            declarations.append(d)
+        i, d = elem.make_scalar_getter()
+        if d:
             impls.append((i, elem.get_wrapper_class_name()))
+            declarations.append((d, elem.get_wrapper_class_name()))
         else:
-            print d
+            print i
 
 
 
-    #for impl in impls:
-    #    print impl
+    _code = make_header(declarations)
+    f = open('forge_v8_point.h', 'w')
+    f.write(('\r\n'.join(_code))
+        .replace('\r', '@')
+        .replace('@', ''))
+    f.close()
 
-    _code = make_wrapper_class(impls)
+    _code = make_source(impls)
     for line in _code:
         print line
 
-    f = open('forge_v8_point.h', 'w')
-    #for line in _code:
-    f.write(('\r\n'.join(_code))
-        #.replace('\n\r\n', '\n')
-        .replace('\r', '@')
-        .replace('@', ''))
-
-    #print(elem.make_scalar_setter())
-    #elif Dart
 
 
 if __name__ == '__main__':
