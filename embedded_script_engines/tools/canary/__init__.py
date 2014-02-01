bfdf9f177e0efca1c97aa66adba6f0e8dfe316cd
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-02-01 17:35:25 +0400

    save

---
+++ embedded_script_engines/tools/canary/__init__.py
@@ -0,0 +1,7 @@
+
+# Other
+from generator.cpp import utils
+
+# Main()
+source = utils.ReadFile('idata/up2D.h', 'r')
+print source
---
+++ embedded_script_engines/tools/idata/up2D.h
@@ -0,0 +1,15 @@
+
+
+/*
+
+*/
+
+class New2D : public Hello {
+public:
+  void SendUpPower() {
+
+  }
+
+  // state
+  int currents[2][4];
+};
