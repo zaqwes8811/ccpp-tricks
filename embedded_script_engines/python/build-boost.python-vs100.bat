f562765417deed2393945aaadeb60533794eb591
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-02-11 22:40:31 +0400

    Edit boost build script.

--- embedded_script_engines/python/build-boost.python-vs100.bat
+++ embedded_script_engines/python/build-boost.python-vs100.bat
@@ -1,9 +1,10 @@
+set VER = msvc-10.0
 bjam ^
   -j2 ^
-  --toolset=msvc-10.0 ^
+  --toolset=%VER% ^
   release debug ^
   --with-python ^
   -sPYTHON_VERSION=2.7 -sPYTHON_ROOT=C:\Python27 ^
   threading=multi link=static link=shared runtime-link=shared ^
-  stage --stagedir="../../lib"
+  stage --stagedir="lib/%VER%"
   pause
