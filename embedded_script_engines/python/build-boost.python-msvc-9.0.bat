0f17f42c25715505f754b1d4dd0cb9ed8cdd7472
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-02-11 22:41:42 +0400

    save and push

---
+++ embedded_script_engines/python/build-boost.python-msvc-9.0.bat
@@ -0,0 +1,10 @@
+set VER = msvc-9.0
+bjam ^
+  -j2 ^
+  --toolset=%VER% ^
+  release debug ^
+  --with-python ^
+  -sPYTHON_VERSION=2.7 -sPYTHON_ROOT=C:\Python27 ^
+  threading=multi link=static link=shared runtime-link=shared ^
+  stage --stagedir="lib/%VER%"
+  pause
