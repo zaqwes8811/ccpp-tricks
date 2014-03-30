78e64c1388358e5704c1469512318314f2b2236c
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-30 10:58:58 +0400

    Do tutorial for protobuf

---
+++ bin/addressbook_gen.bat
@@ -0,0 +1,7 @@
+﻿@echo off
+set SRC_DIR="."
+set DST_DIR=.\\src
+
+rem protoc -I=%SRC_DIR% --cpp_out=%DST_DIR% %SRC_DIR%\\addressbook.proto
+protoc -I=%SRC_DIR% --cpp_out=%DST_DIR% %SRC_DIR%\\addressbook.proto
+pause
