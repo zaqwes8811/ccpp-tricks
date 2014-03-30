225b4df74ecb9e0a73b65dd6260285947b8a5aff
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-30 11:44:43 +0400

    save

--- src/serialize/protobuf_filter_test.cc
+++ src/serialize/protobuf_filter_test.cc
@@ -55,6 +55,37 @@ void PromptForAddress(tutorial::Person* person) {
   }
 }
 
+// Iterates though all people in the AddressBook and prints info about them.
+void ListPeople(const tutorial::AddressBook& address_book) {
+  for (int i = 0; i < address_book.person_size(); i++) {
+    const tutorial::Person& person = address_book.person(i);
+
+    cout << "Person ID: " << person.id() << endl;
+    cout << "  Name: " << person.name() << endl;
+    if (person.has_email()) {
+      cout << "  E-mail address: " << person.email() << endl;
+    }
+
+    for (int j = 0; j < person.phone_size(); j++) {
+      const tutorial::Person::PhoneNumber& phone_number = person.phone(j);
+
+      switch (phone_number.type()) {
+        case tutorial::Person::MOBILE:
+          cout << "  Mobile phone #: ";
+          break;
+        case tutorial::Person::HOME:
+          cout << "  Home phone #: ";
+          break;
+        case tutorial::Person::WORK:
+          cout << "  Work phone #: ";
+          break;
+      }
+      cout << phone_number.number() << endl;
+    }
+  }
+}
+
+
 TEST(Serialize, ProtobufTutorial) {
   // https://developers.google.com/protocol-buffers/docs/cpptutorial
   // Main function:  Reads the entire address book from a file,
