4dfc5f04a584035bfcab5e37d912cfe8659577bc
zaqwes <zaqwes@localhost.localdomain>
2014-04-11 17:00:06 +0400

    Troubles build V8.

--- linux/build_v8_arm.sh
+++ linux/build_v8_arm.sh
@@ -2,17 +2,18 @@
 
 # https://code.google.com/p/v8/wiki/CrossCompilingForARM
 
-export TOOL_PREFIX=/opt/codesourcery/arm-2009q1/bin/arm-none-linux-gnueabi
-#export TOOL_PREFIX=/opt/codesourcery/cross-gcc/arm-2007q1/bin/arm-none-linux-gnueabi
+V=/home/zaqwes/big_device/condesourcery/arm-2009q1
+
+export TOOL_PREFIX=$V/bin/arm-none-linux-gnueabi
 export CXX=$TOOL_PREFIX-g++
 export AR=$TOOL_PREFIX-ar
 export RANLIB=$TOOL_PREFIX-ranlib
 export CC=$TOOL_PREFIX-gcc
 export LD=$TOOL_PREFIX-g++
-#$TOOL_PREFIX-ld
 
-#export CCFLAGS="-march=armv7-a -mtune=cortex-a8 -mfpu=vfp"
-export CCFLAGS="-mcpu=arm926ej-s -march=armv5te"
-export ARM_TARGET_LIB=/opt/codesourcery/arm-2009q1/arm-none-linux-gnueabi/libc
+export CCFLAGS="-march=armv7-a -mtune=cortex-a8 -mfpu=vfp"
+#export CCFLAGS="-mcpu=arm926ej-s -march=armv5te"
+export ARM_TARGET_LIB=$V/arm-none-linux-gnueabi/libc
 
-make arm.release wordsize=32 snapshot=off sample=shell werror=no armfloatabi=softfp
+make arm.release wordsize=32 snapshot=off sample=shell werror=no 
+#armfloatabi=softfp
