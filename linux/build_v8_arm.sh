69a20105a448ee353e16c083e319e604763a7e47
Igor Lugansky <igor.a.lugansky@gmail.com>
2014-04-14 05:22:17 -0400

    Update build_v8_arm.sh

--- linux/build_v8_arm.sh
+++ linux/build_v8_arm.sh
@@ -12,12 +12,19 @@ export CC=$TOOL_PREFIX-gcc
 export LD=$TOOL_PREFIX-g++
 
 #export CCFLAGS="-march=armv7-a -mtune=cortex-a8 -mfpu=vfp"
-export CCFLAGS="-mcpu=arm926ej-s -march=armv5te"
+export CCFLAGS="-mcpu=arm926ej-s -march=armv5te -mno-thumb-interwork"
 export ARM_TARGET_LIB=$V/arm-none-linux-gnueabi/libc
 
 # http://www.wigwag.com/devblog/cross-compile-v8-target-arm/!!
 # https://code.google.com/p/v8/wiki/CrossCompilingForARM
 # -j4 - ?
-make arm.release wordsize=32 snapshot=off sample=shell werror=no armfloatabi=soft
+make arm.debug wordsize=32 snapshot=off sample=shell vfp3=off werror=no armfloatabi=soft armv7=false
+# off max count values
+# wordsize=32 snapshot=off arch=arm vfp3=off armeabi=soft sample=shell
 # armeabi=soft
 #fp
+#
+# Try arm.debug
+# GCC switches: -march=XXX, -mtune=XXX, -mno-thumb-interwork
+# V8 scons options: vfp3=off/on, armeabi=off/on
+# V8 scons snapshot=off  - Later try to move to snapshot=on
