bbb1d325df5a4c23e590804fe29aaeb25f0a6db1
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-10 20:18:57 +0300

    save

--- src/learn/sutter_tasks/mem_and_resources_6_test.cc
+++ src/learn/sutter_tasks/mem_and_resources_6_test.cc
@@ -1,5 +1,26 @@
+// http://www.rsdn.ru/forum/cpp/2810409.1 !!
+
 // Third party
 #include <gtest/gtest.h>
+
+// @Ref
+// 1
+// Error: не определн new and new[]
+class B {
+public:
+    virtual ~B();
+    void operator delete(void*, size_t) throw();
+    void operator delete[](void*, size_t) throw();
+    void f(void*, size_t) throw();
+
+};
+
+class D: public B {
+public:
+    void operator delete(void*) throw();
+    void operator delete[](void*) throw();
+};
+// @Ref
 
 TEST(Sutter, CheckFile) {
 
