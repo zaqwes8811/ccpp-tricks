6f2707d4abf531b6ca0e67167959fe428935378e
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-10 22:24:51 +0300

    save

--- src/learn/sutter_tasks/mem_and_resources_6_test.cc
+++ src/learn/sutter_tasks/mem_and_resources_6_test.cc
@@ -1,4 +1,23 @@
-// http://www.rsdn.ru/forum/cpp/2810409.1 !!
+// Memory types:
+//   http://www.rsdn.ru/forum/cpp/2810409.1 !!
+//
+// Свой new:
+// http://www.rsdn.ru/forum/cpp/359892.flat
+// Есть в "Современном проектировании" Александрески
+//
+// Маерс 55:
+//
+// http://www.amse.ru/courses/cpp2/2011_03_21.html
+// StackOv.:
+//   http://stackoverflow.com/questions/7824264/why-does-operator-deletes-signature-take-two-parameters
+//   http://stackoverflow.com/questions/7149461/why-would-one-replace-default-new-and-delete-operators
+//
+// Standart:
+//   http://stackoverflow.com/questions/7194127/how-should-i-write-iso-c-standard-conformant-custom-new-and-delete-operators/
+//
+// Xackep.ru:
+//   http://www.xakep.ru/post/54481/
+//   http://www.xakep.ru/post/54726/
 
 // C++
 #include <stdexcept>
@@ -32,6 +51,38 @@ public:
     return ::operator new(s);
   }
 };
+
+// TODO: Это типа менеджера памяти?
+class SharedMemory {
+public:
+  static void* Allocate(size_t s) {
+    return 0;  // OsSpecificSharedMemAllocation(s);
+  }
+  static void Deallocate(void* p, int i=0) {
+    // OsSpecificSharedMemDeallocation(p, i);
+  }
+};
+
+class Y {
+public:
+  void * operator new(size_t s, SharedMemory& m) throw(bad_alloc) {
+    return m.Allocate(s);
+  }
+
+  // Он не парный что ли?
+  void operator delete(void* p, SharedMemory& m, int i) throw() {
+    m.Deallocate(p);
+  }
+};
+
+/*
+// ERROR: Переопределен глобальный оператор!!
+void operator delete(void* p) throw() {
+  SharedMemory::Deallocate(p);
+}
+void operator delete(void* p, std::nothrow_t&) throw() {
+  SharedMemory::Deallocate(p);
+}*/
 // @Ref
 
 TEST(Sutter, CheckFile) {
