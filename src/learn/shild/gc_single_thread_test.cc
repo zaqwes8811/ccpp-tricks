2d3998842613cab6889cf242d5f42a5fcea1a3a9
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-13 02:52:39 +0300

    save

--- src/learn/shild/gc_single_thread_test.cc
+++ src/learn/shild/gc_single_thread_test.cc
@@ -1,1 +1,73 @@
+// Note: комментарии в Шилде стр. 31
+#include <iostream>
+#include <list>
+#include <typeinfo>
+#include <cstdlib>
+
+using namespace std;  // Bad in header!!
+
+//#define DISPLAY
+
+class OutOfRangeExc {
+
+};
+
+// Перемещение по какому-то массиву.
+template <class T> class Iter {
+  T *ptr;
+  T *end;
+  T *begin;
+  unsigned length;
+public:
+  Iter() {
+    ptr = end = begin = NULL;
+  }
+
+  Iter(T *p, T *first, T *last) {
+    ptr = p;
+    end = last;
+    begin = first;
+    length = last - first;
+  }
+
+  unsigned size() { return length; }
+
+  T &operator*() {
+    if ((ptr >= end) || (ptr < begin))
+      throw OutOfRangeExc();
+    return *ptr;
+  }
+  T *operator->() {
+    if ((ptr >= end) || (ptr < begin))
+      throw OutOfRangeExc();
+    return ptr;
+  }
+
+  Iter operator--() {
+    ptr--;
+    return *this;
+  }
+
+  Iter operator++(int notused) {
+    T *tmp = ptr;
+    ptr++;
+    return Iter<T>(tmp, begin, end);
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
