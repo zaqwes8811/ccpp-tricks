87b73280be9c9cf2f06e0b62bfb6b54eb53f8a82
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-06-23 21:02:30 +0400

    save

--- src/stl/stl_iter_func_test.cc
+++ src/stl/stl_iter_func_test.cc
@@ -1,9 +1,28 @@
 #include <vector>
 #include <algorithm>
+#include <iostream>
+#include <list>
 
 #include <gtest/gtest.h>
 
 using std::vector;
+using std::cout;
+using std::endl;
+
+template<class T>
+inline void print_elems(const T& coll, const char* optcstr="") {
+  typename T::const_iterator pos;
+  cout << optcstr;
+  for (pos = coll.begin(); pos != coll.end(); ++pos)
+    cout << *pos << ' ';
+  cout << endl;
+}
+
+template<class T>
+inline void insert_elems(T& coll, int first, int last) {
+  for (int i = first; i <= last; ++i)
+    coll.insert(coll.end(), i);
+}
 
 // http://easy-coding.blogspot.ru/2009/07/blog-post_25.html
 // функтор быстрее указателя на функцию, но см. комменты
@@ -38,4 +57,27 @@ TEST(STL, VectorPPProblem) {
 }
 
 /// Util functions
+TEST(STL_Iter, UtilFunc) {
+  using std::list;
+  using std::advance;
 
+  list<int> coll;
+
+  insert_elems(coll, 1, 9);
+
+  list<int>::iterator pos = coll.begin();
+  assert(*pos == 1);
+
+  advance(pos, 3);
+  assert(*pos == 4);
+
+  // distance() - первый должен предш. второму или совпадать с ним
+}
+
+/// Adaptors
+// предбразование в обр. меняет что-то p. 273. Физ поз. сохр., а лог. перемещ
+//
+// Insert Iter. - итератор вывода - реализ. так что вызывает insert or push_backs
+//   and other.
+// ++, -- - фиктивные операции
+// бывает трех типов - нач., конеч. и общие
