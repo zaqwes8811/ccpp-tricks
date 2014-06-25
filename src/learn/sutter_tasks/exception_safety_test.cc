69b9adf8442bb8ae0b9b2e19fcdfc35ca3cb4179
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-06-25 12:10:11 +0400

    save

--- src/learn/sutter_tasks/exception_safety_test.cc
+++ src/learn/sutter_tasks/exception_safety_test.cc
@@ -13,10 +13,39 @@
 //   Сообщения об ошибках
 //   Распространение ошибок
 //   Обработка
+// Важно то что обработчик должен иметь достаточно информации - например пользователь
+//
+// "...обрабатывает сам и остается в согласованном состоянии..."
+// Интересно сделано. Классы исключения включены в интерфейс класса, как внутр. классы
 
+#include <stdexcept>
 
 #include <gtest/gtest.h>
 
+using std::logic_error;
+
+class Session {
+public:
+  // Inner class:
+  // http://bytes.com/topic/c/answers/133914-static-nested-class
+  // http://stackoverflow.com/questions/3846696/nested-classes-c-static-inner-methods-xml-parsing-and-trying-to-populate-vect
+  //
+  // Inh.:
+  // http://stackoverflow.com/questions/15777635/c-calling-base-class-constructors
+  /*static*/ class OpenFailed : public logic_error {
+  public:
+    OpenFailed() : logic_error("") {
+
+    }
+  };
+
+  // Throws: OpenFailed
+  void Open() {
+  }
+};
+
+
 TEST(Sutter, ExceptionBase) {
+  Session::OpenFailed error();
 
 }
