8d9e6052d1df95dfb80b2225c6f5b9fcbc9c3727
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-06-26 23:10:34 +0400

    save

--- src/apps/pkgs_queue_apps.cc
+++ src/apps/pkgs_queue_apps.cc
@@ -12,4 +12,13 @@
 // Info:
 // http://web.stanford.edu/class/cs140/cgi-bin/lecture.php?topic=process
 // http://programmers.stackexchange.com/questions/206962/should-multi-threading-be-used-for-tasks-which-does-not-involve-io-operation
+//
+// coupled - Queue
 // https://www.quantnet.com/threads/c-multithreading-in-boost.10028/
+// http://stackoverflow.com/questions/10139251/shared-queue-in-c
+//
+// concurent queue
+// http://stackoverflow.com/questions/16275112/boost-c-lock-free-queue-vs-shared-queue
+// http://www.alexey-martynov.pp.ru/index.php?data=articles&article=mt-queue
+// http://www.justsoftwaresolutions.co.uk/threading/implementing-a-thread-safe-queue-using-condition-variables.html
+// http://stackoverflow.com/questions/6959023/c-producer-consumer-queue-with-very-fast-and-reliable-handover
