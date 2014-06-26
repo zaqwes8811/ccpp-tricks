0e761a8fd47629ce876daa7d6b3f123812c59450
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-06-26 15:27:25 +0400

    save

--- src/learn/sutter_tasks_new/templ_stl_test.cc
+++ src/learn/sutter_tasks_new/templ_stl_test.cc
@@ -2,10 +2,14 @@
 
 #include <cassert>
 #include <vector>
+#include <iostream>
+#include <algorithm>
 
 #include <gtest/gtest.h>
 
 using std::vector;
+using std::cout;
+using std::endl;
 
 /// Task 1: Vector
 
@@ -14,7 +18,15 @@ void f(vector<int>& v) {
   v.at(0);
 }
 
+template<class Container, class OutputIterator>
+OutputIterator copy_own(const Container& c, OutputIterator result) {
+  return std::copy(c.begin(), c.end(), result);
+}
+
 TEST(SutterNew, Task1) {
+  using std::ostream_iterator;
+  using std::copy;
+
   vector<int> v;
   v.reserve(2);
   //assert(v.capacity() == 2);
@@ -30,16 +42,24 @@ TEST(SutterNew, Task1) {
   // raw
   for (vector<int>::iterator i = v.begin();
        i < v.end(); i++) {
-    *i;
+    cout << *i << endl;
   }
 
   // updated
-  for (vector<int>::const_iterator i=v.begin(),
+  for (vector<int>::const_iterator i=v.begin(),  // const better if read only
       end=v.end();  // reduce extra calc
       i != end;  // < -> !=
       ++i) {
-    *i;
+      cout << *i << '\n';  // endl сбрасывает буффер
   }
+
+  // high level update - вообще дело вкуса
+  // сразу пропадает ряд ошибок.
+  copy(v.begin(), v.end(), ostream_iterator<int>(cout, "\n"));
+
+  // high level - with const_iter
+  copy_own(v, ostream_iterator<int>(cout, "\n"));
+
 }
 
 
