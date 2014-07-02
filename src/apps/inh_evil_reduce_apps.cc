b0a1ab14ce1c1db1d536959a3f54a2d1719c03c6
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-07-02 18:52:59 +0400

    save

--- src/apps/inh_evil_reduce_apps.cc
+++ src/apps/inh_evil_reduce_apps.cc
@@ -1,2 +1,30 @@
 // http://channel9.msdn.com/Events/GoingNative/2013/Inheritance-Is-The-Base-Class-of-Evil
 // TODO: C++98
+#include <string>
+#include <iostream>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+//using object_t = int;  // C++11
+using std::ostream;
+using std::endl;
+using std::string;
+using std::vector;
+
+typedef int object_t;
+typedef vector<object_t> document_t;
+void draw(const object_t& x, ostream& out, size_t position)
+{ out << string(position, ' ') << x << endl; }
+
+void draw(const document_t& x, ostream& out, size_t position)
+{
+  out << string(position, ' ') << "<document>" << endl;
+  // for (const auto& e : x) draw(e, out, position + 2); // C++11
+  out << string(position, ' ') << "</document>" << endl;
+}
+
+
+TEST(Evil, App) {
+
+}
