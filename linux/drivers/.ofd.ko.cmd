9343741843eb232e038cdf83a0518d2c56504f38
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-07-14 13:31:32 +0400

    Build first driver on Debian.

---
+++ linux/drivers/.ofd.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/zaqwes/work/in-the-vicinity-cc/linux/drivers/ofd.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.2.0-4-common/scripts/module-common.lds --build-id  -o /home/zaqwes/work/in-the-vicinity-cc/linux/drivers/ofd.ko /home/zaqwes/work/in-the-vicinity-cc/linux/drivers/ofd.o /home/zaqwes/work/in-the-vicinity-cc/linux/drivers/ofd.mod.o
---
+++ linux/drivers/.tmp_versions/ofd.mod
@@ -0,0 +1,2 @@
+/home/zaqwes/work/in-the-vicinity-cc/linux/drivers/ofd.ko
+/home/zaqwes/work/in-the-vicinity-cc/linux/drivers/ofd.o
