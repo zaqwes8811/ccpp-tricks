23f4faffc038d0d6aacba9dfb0eefdfa1af47103
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-07-17 18:24:57 +0400

    First SQL request.

---
+++ rdb/postrgesql/base.sql
@@ -0,0 +1,12 @@
+﻿-- http://stackoverflow.com/questions/1766046/postgresql-create-table-if-not-exists
+-- create table people (
+--        id              integer,
+--        firstname       varchar(20),
+--        lastname        varchar(20),
+--        phonenumber     char(10)
+--);
+
+insert into people (id, firstname, lastname, phonenumber) values 
+	(1, 'Fred', 'Flin', '123');
+
+select * from people;
