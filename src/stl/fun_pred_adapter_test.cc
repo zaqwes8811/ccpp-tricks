b4650858aa4601a1c001d78e331d537936696234
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-08-18 10:51:09 +0400

    save

---
+++ src/stl/fun_pred_adapter_test.cc
@@ -0,0 +1,6 @@
+// http://www.codeproject.com/Tips/537403/Cplusplus-Prefer-bind-to-bind-st-and-bind-nd
+// http://www-h.eng.cam.ac.uk/help/tpl/languages/C++/bind.html
+// bind2nd - op(a1, A2_conn)
+// bind1st - op(A1_conn, a2)
+//
+// Note: В алгоритм можно передать и по ссылке, но тогда нужно полностью специфицировать шаблон.
