55d5418cff7627313643a252abb557c9bff8e6df
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-08-30 14:35:40 +0400

    Add pool task.

--- learn_tricks/sutter/eff_conc_test.cc
+++ learn_tricks/sutter/eff_conc_test.cc
@@ -1,5 +1,46 @@
 // Eff conc.
+// http://herbsutter.com/2009/11/11/effective-concurrency-prefer-structured-lifetimes-%E2%80%93-local-nested-bounded-deterministic/
 
+#include <gtest/gtest.h>
+
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
 
 // TODO: Use Threads Correctly = Isolation + Asynchronous Messages
 // http://www.drdobbs.com/parallel/use-threads-correctly-isolation-asynch/215900465
+
+// TODO: Use Thread Pools Correctly: Keep Tasks Short and Nonblocking
+// http://stackoverflow.com/questions/19500404/how-to-create-a-thread-pool-using-boost-in-c
+//
+// http://thisthread.blogspot.co.il/2011/04/multithreading-with-asio.html
+namespace
+{
+  boost::mutex mio; // 1
+ 
+  void logMsg(char* reason, int i)
+  {
+    boost::lock_guard<boost::mutex> lock(mio); // 2.
+    std::cout << reason << " for " << i << " at " << std::time(0) << std::endl;
+  }
+ 
+  void jobOne(int secs) // 3.
+  {
+    logMsg("Start jobOne", secs);
+    boost::this_thread::sleep(boost::posix_time::millisec(1000 * secs));
+    logMsg("End jobOne", secs);
+  }
+ 
+  void jobTwo(int millisecs)
+  {
+    logMsg("Start jobTwo", millisecs);
+    boost::this_thread::sleep(boost::posix_time::millisec(millisecs));
+    logMsg("End jobTwo", millisecs);
+  }
+}
+
+// Pools
+//   http://thisthread.blogspot.co.il/2011/04/multithreading-with-asio.html
+//   http://stackoverflow.com/questions/16677287/boostthreadpoolpool-vs-boostthread-group
+//   http://stackoverflow.com/questions/19500404/how-to-create-a-thread-pool-using-boost-in-c
+//   http://techiesaint.wordpress.com/2012/12/16/implementing-threadpool-pattern-in-c-using-boostthread-pool/
+//   http://www.tonicebrian.com/2012/05/23/thread-pool-in-c/
--- learn_tricks/sutter/sutter.pro
+++ learn_tricks/sutter/sutter.pro
@@ -7,16 +7,20 @@ OBJECTS_DIR = obj
 
 QMAKE_CXXFLAGS += -O2 -std=c++11 -Wall -Wno-deprecated -pg
 
+_3RD_PARTY=../../3rdparty
+BOOST=/home/zaqwes/work/third_party/boost_1_55_0
 
 SOURCES += \
   ./ch9_conc_parall_test.cc \
   eff_conc_test.cc \
-  ../../../third_party/gmock-1.6.0/fused-src/gmock-gtest-all.cc \
+  $$_3RD_PARTY/gmock-1.6.0/fused-src/gmock-gtest-all.cc \
   ../../launcher.cc
 
-INCLUDEPATH += /home/zaqwes/work/third_party/source_release \
-  /home/zaqwes/work/third_party/boost_1_55_0 \
-  ../../../third_party/gmock-1.6.0/fused-src/ \
+INCLUDEPATH += \
+  /home/zaqwes/work/third_party/source_release \
+  $$BOOST \
+  $$_3RD_PARTY/gmock-1.6.0/fused-src/ \
   ../../
 
 LIBS += -lpthread -lrt -ltbb
+LIBS += -L$$BOOST/stage/lib -lboost_thread -lboost_system
