137be4ca5e1824f14aaec991c8c50d5f53741c2a
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-09-01 13:50:58 +0400

    save

--- learn_tricks/sutter/eff_conc_test.cc
+++ learn_tricks/sutter/eff_conc_test.cc
@@ -13,6 +13,10 @@
 // TODO: Use Thread Pools Correctly: Keep Tasks Short and Nonblocking
 // http://stackoverflow.com/questions/3988128/c-thread-pool - good pool
 
+// TODO: Sharing Is the Root of All Contention
+// Even with races no fast in multicore system!
+// "Yes, using locks and CAS incurs waiting and overhead expenses, 
+// but those all arise from the root cause of having a mutable shared object at all."
 
 // TODO: Avoid Exposing Concurrency: Hide It Inside Synchronous Methods
 // А как быть со всякими асинхоронными api?
@@ -23,3 +27,48 @@
 // thread interrurpt - но а если объект потока не доступен?
 //
 //  Join the thread before destroying or use an scoped thread.
+
+// TODO: Effective Concurrency: Eliminate False Sharing
+// http://habrahabr.ru/company/intel/blog/143446/
+/*
+// Example 1: Simple parallel version (flawed)
+//
+int result[P];
+// Each of P parallel workers processes 1/P-th
+// of the data; the p-th worker records its
+// partial count in result[p]
+for( int p = 0; p < P; ++p )
+  pool.run( [&,p] {
+    result[p] = 0;
+    int chunkSize = DIM/P + 1;
+    int myStart = p * chunkSize;
+    int myEnd = min( myStart+chunkSize, DIM );
+    for( int i = myStart; i < myEnd; ++i )
+      for( int j = 0; j < DIM; ++j )
+        if( matrix[i*DIM + j] % 2 != 0 )
+          ++result[p];
+  } );
+// Wait for the parallel work to complete…
+pool.join();
+// Finally, do the sequential "reduction" step
+// to combine the results
+odds = 0;
+
+// Что-то похожее на CUDA opt
+for( int p = 0; p < P; ++p )
+  odds += result[p];
+
+// V2
+for( int p = 0; p < P; ++p )
+   pool.run( [&,p] {
+   int count = 0;
+   int chunkSize = DIM/P + 1;
+   int myStart = p * chunkSize;
+   int myEnd = min( myStart+chunkSize, DIM );
+   for( int i = myStart; i < myEnd; ++i )
+      for( int j = 0; j < DIM; ++j )
+        if( matrix[i*DIM + j] % 2 != 0 )
+          ++count;
+   result[p] = count;
+} );
+*/
