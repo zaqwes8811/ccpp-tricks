c628c6cdb8b47debbb86b1e16a990d9f6db500f1
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-09-02 21:52:17 +0400

    try cmake.

---
+++ tools/learn_cmake/build/a.app.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/../a.app.dir/home/zaqwes/work/in-the-vicinity-cc/launcher.cc.o"
+  "../a.app.pdb"
+  "../a.app"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/../a.app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
