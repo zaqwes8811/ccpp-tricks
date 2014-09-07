35c8d4ce5d9f3e8803ed667785fe94775c950d47
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-09-07 17:50:13 +0400

    Drivers installed for other kernel.

--- hpc/cuda_nvidia_course/cs344/HW5/build/cmake_install.cmake
+++ hpc/cuda_nvidia_course/cs344/HW5/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/zaqwes/work/in-the-vicinity-cc/hpc/cuda_nvidia_course/src/cs344/HW5
+# Install script for directory: /home/zaqwes/work/in-the-vicinity-cc/hpc/cuda_nvidia_course/cs344/HW5
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,7 +38,7 @@ ELSE(CMAKE_INSTALL_COMPONENT)
   SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 ENDIF(CMAKE_INSTALL_COMPONENT)
 
-FILE(WRITE "/home/zaqwes/work/in-the-vicinity-cc/hpc/cuda_nvidia_course/src/cs344/HW5/build/${CMAKE_INSTALL_MANIFEST}" "")
+FILE(WRITE "/home/zaqwes/work/in-the-vicinity-cc/hpc/cuda_nvidia_course/cs344/HW5/build/${CMAKE_INSTALL_MANIFEST}" "")
 FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "/home/zaqwes/work/in-the-vicinity-cc/hpc/cuda_nvidia_course/src/cs344/HW5/build/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  FILE(APPEND "/home/zaqwes/work/in-the-vicinity-cc/hpc/cuda_nvidia_course/cs344/HW5/build/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 ENDFOREACH(file)
