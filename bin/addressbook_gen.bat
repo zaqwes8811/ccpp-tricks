c8117d4b2f70c808de0ea2029bc749ae1b2142b0
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-09-17 14:58:55 +0400

    big phy

--- bin/addressbook_gen.bat
+++
@@ -1,7 +0,0 @@
-﻿@echo off
-set SRC_DIR="."
-set DST_DIR=..\\src\\serialize
-
-rem protoc -I=%SRC_DIR% --cpp_out=%DST_DIR% %SRC_DIR%\\addressbook.proto
-protoc -I=%SRC_DIR% --cpp_out=%DST_DIR% %SRC_DIR%\\addressbook.proto
-pause
