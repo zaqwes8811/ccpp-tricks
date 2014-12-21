22e757133f34a4a98cb825625f1faf89050562a2
zaqwes <igor.a.lugansky@gmail.com>
2014-12-22 00:07:12 +0400

    save

--- tests/unit/async-parallel-computing/safe_data_structures/queue_test.cc
+++ tests/unit/async-parallel-computing/safe_data_structures/queue_test.cc
@@ -64,6 +64,7 @@ void push(const T& t){
 #include <boost/thread/locks.hpp>
 #include <boost/thread/lock_algorithms.hpp>
 #include <boost/noncopyable.hpp>
+#include <boost/range/end.hpp>
 
 #include <list>
 #include <ostream>
@@ -122,143 +123,6 @@ private:
 }
 
 namespace fix_extern_space {
-/** Pro
-   http://stackoverflow.com/questions/20488854/implementation-of-a-bounded-blocking-queue-using-c
-
- https://www.threadingbuildingblocks.org/docs/help/reference/containers_overview/concurrent_bounded_queue_cls.htm
- Assume all includes are there
-
-  http://www.boost.org/doc/libs/1_35_0/doc/html/thread/synchronization.html#thread.synchronization.condvar_ref.condition_variable_any
-*/
-struct BoundedBlockingQueueTerminateException
-    : virtual std::exception,
-      virtual boost::exception { };
-
-// http://stackoverflow.com/questions/5018783/c-pthread-blocking-queue-deadlock-i-think
-template<typename T>
-class bounded_blocking_queue : public boost::noncopyable {
-private:
-  std::queue<T> m_q;  // FIXME: to list
-  boost::mutex m_mtx;
-  boost::condition_variable_any m_pop_cv; // q.empty() condition
-  boost::condition_variable_any m_push_cv; // q.size() == size condition
-  int m_nblocked_pop;
-  int m_nblocked_push;
-  bool m_stopped;
-  int m_size;
-
-public:
-  // types
-  typedef T value_type;
-  //typedef A allocator_type;
-  typedef T& reference;
-  typedef const T& const_reference;
-
-  bounded_blocking_queue(int size)
-      : m_size(size), m_nblocked_pop(0), m_nblocked_push(0), m_stopped(false) {
-    if (m_size < 1){
-        // BOOST_THROW_EXCEPTION
-    }
-  }
-
-  ~bounded_blocking_queue() {
-    stop(true);
-  }
-
-  bool empty() {
-    boost::mutex::scoped_lock lock(m_mtx);
-    return m_q.empty();
-  }
-
-  std::size_t size() {
-    boost::mutex::scoped_lock lock(m_mtx);
-    return m_q.size();
-  }
-
-  bool try_push(const T& elem) {
-    {
-      boost::mutex::scoped_lock lock(m_mtx);
-      if (m_q.size() == m_size)//size)
-          return false;
-
-      m_q.push(elem);  // bad
-    }
-    m_pop_cv.notify_one();
-    return true;
-  }
-
-private:
-  void push(const T& item) {
-    {
-      boost::mutex::scoped_lock lock(m_mtx);
-
-      ++m_nblocked_push;
-      while (!m_stopped && m_q.size() == size)
-        m_push_cv.wait(lock);
-      --m_nblocked_push;
-
-      if (m_stopped) {
-        m_push_cv.notify_all();
-        BOOST_THROW_EXCEPTION(BoundedBlockingQueueTerminateException());
-      }
-
-      m_q.push(item);
-    }
-    m_pop_cv.notify_one();
-  }
-
-public:
-  bool try_pop(T& popped) {
-    {
-      boost::mutex::scoped_lock lock(m_mtx);
-      if (m_q.empty())
-          return false;
-
-      popped = m_q.front();
-      m_q.pop();
-    }
-    m_push_cv.notify_one();
-    return true;
-  }
-
-  /**
-    \brief blocked call
-  */
-private:
-  void pop(T& popped) {
-    boost::mutex::scoped_lock lock(m_mtx);
-
-    ++m_nblocked_pop;
-    while (!m_stopped && m_q.empty())
-        m_pop_cv.wait(lock);
-    --m_nblocked_pop;
-
-    if (m_stopped) {
-      m_pop_cv.notify_all();
-      BOOST_THROW_EXCEPTION(BoundedBlockingQueueTerminateException());
-    }
-
-    popped = m_q.front();
-    m_q.pop();
-    m_push_cv.notify_one();
-  }
-
-public:
-  void stop(bool wait) {
-    boost::mutex::scoped_lock lock(m_mtx);
-    m_stopped = true;
-    m_pop_cv.notify_all();
-    m_push_cv.notify_all();
-
-    if (wait) {
-      while (m_nblocked_pop)
-        m_pop_cv.wait(lock);
-      while (m_nblocked_push)
-        m_push_cv.wait(lock);
-    }
-  }
-};
-
 //
 // TODO: очередь от Шена
 // http://channel9.msdn.com/Events/GoingNative/2013/Cpp-Seasoning
@@ -288,9 +152,164 @@ public:
   // ...
 };
 
+/** Pro
+   http://stackoverflow.com/questions/20488854/implementation-of-a-bounded-blocking-queue-using-c
+
+ https://www.threadingbuildingblocks.org/docs/help/reference/containers_overview/concurrent_bounded_queue_cls.htm
+ Assume all includes are there
+
+  http://www.boost.org/doc/libs/1_35_0/doc/html/thread/synchronization.html#thread.synchronization.condvar_ref.condition_variable_any
+*/
+struct BoundedBlockingQueueTerminateException
+    : virtual std::exception,
+      virtual boost::exception { };
+
+// http://stackoverflow.com/questions/5018783/c-pthread-blocking-queue-deadlock-i-think
+/**
+  \param T - assign operator, copy ctor
+
+*/
+template<typename T>
+class bounded_blocking_queue : public boost::noncopyable {
+public:
+  // types
+  typedef T value_type;
+  //typedef A allocator_type;
+  typedef T& reference;
+  typedef const T& const_reference;
+
+  explicit bounded_blocking_queue(int size)
+      : cm_size(size), m_nblocked_pop(0), m_nblocked_push(0)
+      , m_with_blocked(false)
+      , m_stopped(false)
+  {
+    if (cm_size < 1){
+        // BOOST_THROW_EXCEPTION
+    }
+  }
+
+  ~bounded_blocking_queue() {
+    stop(true);
+  }
+
+  bool empty() {
+    boost::mutex::scoped_lock lock(m_mtx);
+    return m_q.empty();
+  }
+
+  std::size_t size() {
+    boost::mutex::scoped_lock lock(m_mtx);
+    return m_q.size();
+  }
+
+  bool try_push(const T& x) {
+    list<T> tmp;
+    tmp.push_back(x);
+
+    {
+      boost::mutex::scoped_lock lock(m_mtx);
+      if (m_q.size() == cm_size)//size)
+          return false;
+
+      //m_q.push(x);  // bad
+      m_q.splice(boost::end(m_q), tmp);
+      //m_q.splice(m_q.end(), tmp);
+    }
+    //if (m_with_blocked) m_pop_cv.notify_one();
+    return true;
+  }
+
+  bool try_pop(T& popped) {
+    {
+      boost::mutex::scoped_lock lock(m_mtx);
+      if (m_q.empty())
+          return false;
+
+      popped = m_q.front();
+      m_q.pop_front();  // pop()
+    }
+    //if (m_with_blocked) m_push_cv.notify_one();
+    return true;
+  }
+
+private:
+  void stop(bool wait) {
+    boost::mutex::scoped_lock lock(m_mtx);
+    m_stopped = true;
+    m_pop_cv.notify_all();
+    m_push_cv.notify_all();
+
+    if (wait) {
+      while (m_nblocked_pop)
+        m_pop_cv.wait(lock);
+      while (m_nblocked_push)
+        m_push_cv.wait(lock);
+    }
+  }
+
+  /**
+    \brief blocked call
+  */
+  void wait_and_pop(T& popped) {
+    boost::mutex::scoped_lock lock(m_mtx);
+
+    ++m_nblocked_pop;
+
+    while (!m_stopped && m_q.empty())
+      m_pop_cv.wait(lock);
+
+    --m_nblocked_pop;
+
+    if (m_stopped) {
+      m_pop_cv.notify_all();
+      BOOST_THROW_EXCEPTION(BoundedBlockingQueueTerminateException());
+    }
+
+    popped = m_q.front();
+    m_q.pop_front();//pop();
+
+    m_push_cv.notify_one();  // strange
+  }
+
+  void wait_and_push(const T& item) {
+    {
+      boost::mutex::scoped_lock lock(m_mtx);
+
+      ++m_nblocked_push;
+      while (!m_stopped && m_q.size() == size)
+        m_push_cv.wait(lock);
+      --m_nblocked_push;
+
+      if (m_stopped) {
+        m_push_cv.notify_all();
+        BOOST_THROW_EXCEPTION(BoundedBlockingQueueTerminateException());
+      }
+
+      m_q.push(item);
+    }
+    m_pop_cv.notify_one();
+  }
+
+private:
+  std::
+  //queue
+  list
+  <T> m_q;  // FIXME: to list
+  boost::mutex m_mtx;
+  boost::condition_variable_any m_pop_cv; // q.empty() condition
+  boost::condition_variable_any m_push_cv; // q.size() == size condition
+  int m_nblocked_pop;
+  int m_nblocked_push;
+  bool m_stopped;
+  const int cm_size;
+  const bool m_with_blocked;
+};
 }  // space
 
-TEST(DS, TBBAssignTest) {
-
+TEST(ThreadSafeDS, TBBAssignTest) {
+  fix_extern_space::bounded_blocking_queue<int> q(10);
+  int r = 0;
+  EXPECT_TRUE(q.try_push(r));
+  EXPECT_TRUE(q.try_pop(r));
 }
 
