b5e6cee6a148061e988f54116f4ed6c9dd66b27e
zaqwes <igor.a.lugansky@gmail.com>
2015-01-31 21:53:45 +0400

    move async parallel code

--- source/async-parallel-computing/intel_hpc_course/overview_test.cc
+++
@@ -1,7 +0,0 @@
-// https://software.intel.com/ru-ru/articles/report-on-mtl
-//
-// Projects: 
-//   http://code.google.com/p/hpcource/
-//   http://code.google.com/p/hpcource/wiki/Tasks
-//
-// Task: https://software.intel.com/ru-ru/articles/connected-components-of-a-graph
--- tests/unit/async-parallel-computing/top_core_test.cc
+++
@@ -1,9 +0,0 @@
-// Dynamic parallelism NVIDIA:
-//   https://www.udacity.com/course/viewer#!/c-cs344/l-137986669/m-137938845
-//
-// Src: Bulk parallelism
-// Nested parallelism
-// Task parallelism
-// Recursive parallelism
-// 
-// Compossability - parent-child
