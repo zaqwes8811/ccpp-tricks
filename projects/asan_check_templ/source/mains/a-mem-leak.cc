5ea54b346b105385f5eae8523f54ee2fd3216601
zaqwes <igor.a.lugansky@gmail.com>
2015-02-11 04:08:44 -0600

    save

--- projects/asan_check_templ/source/mains/a-mem-leak.cc
+++ projects/asan_check_templ/source/mains/a-mem-leak.cc
@@ -1,6 +1,8 @@
 
 // Need options
 // ASAN_OPTIONS="detect_leaks=1 symbolize=1" out/Release/base_unittests
+// FIXME: must work
+//   https://code.google.com/p/address-sanitizer/wiki/LeakSanitizer
 
 int *g = new int;
 
