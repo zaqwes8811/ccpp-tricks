fe8551767001a9ee12534f06dd372d1c34b5cdb1
zaqwes <igor.a.lugansky@gmail.com>
2015-02-12 15:26:33 +0300

    tsan not work with boost. clang 3.4

--- projects/xsan-pack/tsan_check_templ/source/mains/launcher_all.cc
+++ projects/xsan-pack/tsan_check_templ/source/mains/launcher_all.cc
@@ -1,10 +1,20 @@
-#include <thread>
+//#include <thread>
+
+#include <boost/thread.hpp>
 
 // FIXME: use boost::thread. Need rebuild boost and libstdc++?
+// http://boost.2283326.n4.nabble.com/clang-3-6-thread-sanitizer-complains-on-shared-ptr-operations-td4671416.html
+// Races not founded
+
+void f(int& X) {
+  X = 42;
+  printf("hello");
+}
 
 int main() {
     int X;
-    std::thread t([&] { X = 42;});
+    // not working
+    boost::thread t(boost::bind(&::f, boost::ref(X)));
     X = 43;
     t.join();
 }
