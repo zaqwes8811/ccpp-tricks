211cc0d4bcd2e29f95572d9d0bb29fe5ff811932
zaqwes <igor.a.lugansky@gmail.com>
2015-02-12 15:44:03 +0300

    save

--- projects/xsan-pack/tsan_check_templ/source/mains/launcher_all.cc
+++ projects/xsan-pack/tsan_check_templ/source/mains/launcher_all.cc
@@ -1,10 +1,13 @@
-//#include <thread>
+#include <thread>
 
 #include <boost/thread.hpp>
 
 // FIXME: use boost::thread. Need rebuild boost and libstdc++?
 // http://boost.2283326.n4.nabble.com/clang-3-6-thread-sanitizer-complains-on-shared-ptr-operations-td4671416.html
 // Races not founded
+//
+// readelf -d a.out | grep NEEDED
+// FIXME: may be wring linking?
 
 void f(int& X) {
   X = 42;
@@ -15,6 +18,8 @@ int main() {
     int X;
     // not working
     boost::thread t(boost::bind(&::f, boost::ref(X)));
+    //std::thread t1([&] { X = 44; });
     X = 43;
     t.join();
+    //t1.join();
 }
