5bcfdba63039b196ef1ba32b255d6ebcaee6fd59
zaqwes <igor.a.lugansky@gmail.com>
2015-02-12 15:54:35 +0300

    can't detect data races if user boost.thread

--- projects/xsan-pack/tsan_check_templ/source/mains/launcher_all.cc
+++ projects/xsan-pack/tsan_check_templ/source/mains/launcher_all.cc
@@ -8,6 +8,26 @@
 //
 // readelf -d a.out | grep NEEDED
 // FIXME: may be wring linking?
+//
+// ldd - all right
+/**
+linux-vdso.so.1 =>  (0x00007fff9d0fe000)
+    libboost_system.so.1.57.0 => /opt/big-3rdparty/boost_1_57_0/clang_libx64/lib/libboost_system.so.1.57.0 (0x00007f762d3a1000)
+    libboost_thread.so.1.57.0 => /opt/big-3rdparty/boost_1_57_0/clang_libx64/lib/libboost_thread.so.1.57.0 (0x00007f762d174000)
+    libpthread.so.0 => /lib/x86_64-linux-gnu/libpthread.so.0 (0x00007f762cf3f000)
+    librt.so.1 => /lib/x86_64-linux-gnu/librt.so.1 (0x00007f762cd37000)
+    libdl.so.2 => /lib/x86_64-linux-gnu/libdl.so.2 (0x00007f762cb33000)
+    libm.so.6 => /lib/x86_64-linux-gnu/libm.so.6 (0x00007f762c82c000)
+    libstdc++.so.6 => /usr/lib/x86_64-linux-gnu/libstdc++.so.6 (0x00007f762c528000)
+    libgcc_s.so.1 => /lib/x86_64-linux-gnu/libgcc_s.so.1 (0x00007f762c312000)
+    libc.so.6 => /lib/x86_64-linux-gnu/libc.so.6 (0x00007f762bf4b000)
+    /lib64/ld-linux-x86-64.so.2 (0x00007f762ec9c000)
+
+*/
+
+// FIXME: old clang?
+// FIXME: gcc?
+// FIXME: can't detect
 
 void f(int& X) {
   X = 42;
