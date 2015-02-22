8cb00cf84e8935e87fdf5bb63efc7581d092ef10
zaqwes <igor.a.lugansky@gmail.com>
2015-02-22 22:03:42 +0300

    phy

--- projects/cc_core_and_stl_98_11/tests/lang_core/cc11_base_features_test.cc
+++ projects/cc_core_and_stl_98_11/tests/lang_core/cc11_base_features_test.cc
@@ -1,3 +1,32 @@
+#include <gtest/gtest.h>
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+TEST(Lava, Alg) {
+  int myints[] = {10,20,30,30,20,10,10,20};
+  vector<int> v(myints,myints+8);           // 10 20 30 30 20 10 10 20
+
+  // http://channel9.msdn.com/Series/C9-Lectures-Stephan-T-Lavavej-Standard-Template-Library-STL-/C9-Lectures-Stephan-T-Lavavej-Standard-Template-Library-STL-7-of-n
+  // really use for search
+  sort(v.begin(), v.end());                // 10 10 10 20 20 20 30 30
+
+  // but! find nearst
+  bool contain = std::binary_search(v.begin(), v.end(), 20);
+  auto low = std::lower_bound(v.begin(), v.end(), 20);
+  std::cout << "at position " << (low - v.begin()) << '\n';
+  auto up = std::upper_bound(v.begin(), v.end(), 20);
+  vector<int> slice(low, up);
+  for_each(slice.begin(), slice.end(), [] (int e) { cout << e << " "; });
+  cout << endl;
+
+  // Совет 45. Различайте алгоритмы count, find, - крит равенства
+  // binary_search, lower_bound, upper_bound и equal_range - критерий эквил
+}
+
 // Crosscompilation:
 // http://stackoverflow.com/questions/20574808/how-to-check-arm-none-linux-gnueabi-g-support-for-c11
 // http://blog.worldofcoding.com/2014/05/cross-compiling-c11-without-going-madder.html
@@ -11,20 +40,14 @@
 // !!!https://cloudtips.org/introduction-to-rvalue-references.html
 //
 // http://sergeyteplyakov.blogspot.ru/2012/05/c-11-faq.html
-
-#include <gtest/gtest.h>
-
-#include <vector>
-#include <algorithm>
-
 namespace {
 // Cpp-Seasoning - где нужно move - не очень то где - конструктор и assign
 // "Some sink.."
 // http://stackoverflow.com/questions/18203201/is-the-introduction-of-rvalue-references-actually-useful
 // http://stackoverflow.com/questions/5481539/what-does-t-double-ampersand-mean-in-c11/5481588#5481588
-  
+
 // http://en.wikipedia.org/wiki/C++11#Explicitly_defaulted_and_deleted_special_member_functions
-// 
+//
 // Move assign and ctor:
 //   http://blog.smartbear.com/c-plus-plus/c11-tutorial-introducing-the-move-constructor-and-the-move-assignment-operator/
 //   http://en.cppreference.com/w/cpp/language/move_constructor
@@ -40,13 +63,13 @@ namespace {
 // TODO: компилятор как-то различает lvalue from rvalue?
 // http://meetingcpp.com/index.php/br/items/c1y-move-copy-destructors.html
 class MemoryPage {
-  MemoryPage(MemoryPage&& other) 
+  MemoryPage(MemoryPage&& other)
   //: size(0), buf(nullptr)
   {
   // pilfer other’s resource
   //size=other.size;
   //buf=other.buf;
-  
+
   /// DANGER: reset other - опустошает агрументы!!
   // TODO: А как быть деструктору?
   //other.size=0;
@@ -55,7 +78,7 @@ class MemoryPage {
 
 private:
   MemoryPage& operator=(MemoryPage&& other) = default;
-  
+
 };
 
 // Еще что-то про uniaue_ptr http://www.informit.com/guides/content.aspx?g=cplusplus&seqNum=495
@@ -71,3 +94,4 @@ TEST(Cpp11, SimpleLambda) {
     total += x;
   });
 }
+
--- projects/cc_core_and_stl_98_11/tests/stl/subtle_moments_test.cc
+++ projects/cc_core_and_stl_98_11/tests/stl/subtle_moments_test.cc
@@ -2,6 +2,10 @@
 
 #include <gtest/gtest.h>
 
+#include <algorithm>
+
+using namespace std;
+
 TEST(Stl, EqAndEq) {
   // равенство не тоже самое что эквивалентность - см. в мейсере 173
   //   find and std::insert
@@ -12,8 +16,17 @@ TEST(Stl, EqAndEq) {
   // используемому ассоциативным контейнером с, если ни один из них не предшествует другому в
   // порядке сортировки с."
 
+  // http://channel9.msdn.com/Series/C9-Lectures-Stephan-T-Lavavej-Standard-Template-Library-STL-/C9-Lectures-Stephan-T-Lavavej-Standard-Template-Library-STL-7-of-n
+  // "save sorted order"
+
   int w1 = 0;
   int w2 = 1;
 
   bool is_equal = !(w1 < w2) && !(w2 < w1);
+
+  // better equal
+  //std::e
+
+  //op < // good
+  //op <= // BAD  x comp x must be FALSE
 }
