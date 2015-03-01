11d43cec37f2c6a76f5bef580584a393dd73f7b9
zaqwes <igor.a.lugansky@gmail.com>
2015-03-01 19:47:01 +0300

    save

---
+++ projects/gc_and_alloc/own_alloc_test.cc
@@ -0,0 +1,6 @@
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2007/n2271.html
+// http://stackoverflow.com/questions/826569/compelling-examples-of-custom-c-allocators
+
+// Theory:
+// http://blogs.msdn.com/b/calvin_hsia/archive/2010/03/16/9979985.aspx
+// !! http://blogs.msdn.com/b/vcblog/archive/2008/08/28/the-mallocator.aspx
