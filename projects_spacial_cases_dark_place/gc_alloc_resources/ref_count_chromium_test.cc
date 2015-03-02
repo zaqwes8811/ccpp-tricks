4291e130d9afa2e3c480b8ddc1ad15f79c59337f
zaqwes <igor.a.lugansky@gmail.com>
2015-03-02 16:03:26 +0300

    save

--- projects_spacial_cases_dark_place/gc_alloc_resources/ref_count_chromium_test.cc
+++ projects_spacial_cases_dark_place/gc_alloc_resources/ref_count_chromium_test.cc
@@ -1,1 +1,12 @@
 // dtor is private
+
+// http://www.chromium.org/developers/smart-pointer-guidelines
+// "scoped_refptr is used for reference-counted objects, though normally you should avoid these (see below"
+//
+// "Ref-counted objects - use scoped_refptr; but better yet, rethink your design.  "
+//
+// Sean Parent: shared ptr is evil
+// FIXME: one shared and weak is better?
+
+// Guide:
+//   http://stackoverflow.com/questions/106508/what-is-a-smart-pointer-and-when-should-i-use-one
