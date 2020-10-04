3fb4c5f2158cfeec8760890f1bf784b73b90583f
zaqwes8811 <igor.a.lugansky@gmail.com>
2020-10-04 14:51:38 +0300

    save

---
+++ new_cpp/main.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// task:
+// мовабле передать дальшы, но это поле класса
+
+// task:
+// передать лямбду, с минимумом копиования
+
+// task:
+// move to lamda
+
+int main() {
+
+	return 0;
+}
