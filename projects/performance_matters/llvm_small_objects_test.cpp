1f5ff2423c3f68c5862e25c6f79afb82c54bc5f6
Igor Lugansky <ilugansky@arrival.com>
2022-01-04 00:42:57 +0300

    WIP: added some files

--- projects/performance_matters/llvm_small_objects_test.cpp
+++ projects/performance_matters/llvm_small_objects_test.cpp
@@ -3,3 +3,12 @@
 //
 
 #include <llvm/ADT/SmallVector.h>
+//#include <llvm/ADT/DenseMap.h>
+//#include <llvm/ADT/ImmutableSet.h>
+//#include <llvm/ADT/ImmutableMap.h>
+
+#include <gtest/gtest.h>
+
+TEST(LlvmDataStructuresTest, SmallVector) {
+    auto sv = llvm::SmallVector<int, 4>();
+}
