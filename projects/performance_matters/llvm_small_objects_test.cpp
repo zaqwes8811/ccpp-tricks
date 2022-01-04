4edfb7da1161260244b69e059a1b5391ff4e4c20
Igor Lugansky <ilugansky@arrival.com>
2022-01-04 19:54:57 +0300

    WIP: sep code a bit

--- projects/performance_matters/llvm_small_objects_test.cpp
+++ projects/performance_matters/llvm_small_objects_test.cpp
@@ -3,12 +3,23 @@
 //
 
 #include <llvm/ADT/SmallVector.h>
-//#include <llvm/ADT/DenseMap.h>
+#include <llvm/ADT/DenseMap.h>
+#include <llvm/ADT/SmallSet.h>
+#include <llvm/ADT/SmallPtrSet.h>
 //#include <llvm/ADT/ImmutableSet.h>
 //#include <llvm/ADT/ImmutableMap.h>
+//#include <llvm/ADT/Imm/
 
 #include <gtest/gtest.h>
 
 TEST(LlvmDataStructuresTest, SmallVector) {
     auto sv = llvm::SmallVector<int, 4>();
+}
+
+TEST(LlvmDataStructuresTest, DenseMap) {
+    auto sv = llvm::DenseMap<int, int>();
+}
+
+TEST(LlvmDataStructuresTest, ImmutableMap) {
+//    auto sv = llvm::ImmutableMap<int, int>({1, 1});  // Need lot of code to link
 }
