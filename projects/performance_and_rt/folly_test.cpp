d50b6133ba354b2865e2be2885ebde3966d09932
Igor Lugansky <ilugansky@arrival.com>
2022-02-02 23:46:27 +0300

    WIP: umsg

--- projects/performance_and_rt/folly_test.cpp
+++ projects/performance_and_rt/folly_test.cpp
@@ -4,6 +4,71 @@
 
 // https://www.youtube.com/watch?v=SGdfPextuAU&ab_channel=CppNow
 
+#include <any>
+#include <memory>
+
+using UnifiedMessage = std::any;
+using SharedMessage = std::shared_ptr<UnifiedMessage>;
+using WeakMessage = std::weak_ptr<UnifiedMessage>;
+using UniqueMessage = std::unique_ptr<UnifiedMessage>;
+using IpcSharedMessage = std::unique_ptr<UnifiedMessage>;  // Special counting
+
+template<typename T>
+class UnifiedMessageView {
+public:
+    UnifiedMessageView(SharedMessage msg) : weak_msg_{msg} {
+        data_ptr_ = std::any_cast<T>(msg.get());
+    }
+
+    T* get() {
+        auto c = weak_msg_.lock();
+        if (c) {
+            return data_ptr_;
+        }
+        return nullptr;
+    }
+
+private:
+    T* data_ptr_{};
+    WeakMessage weak_msg_{};
+};
+
+template<typename T>
+T* as(UnifiedMessage& msg) {
+    try {
+        return std::any_cast<T>(&msg);
+    } catch (...) {
+        return nullptr;
+    }
+}
+
+template<typename T>
+std::unique_ptr<UnifiedMessageView<T>> asView(SharedMessage& msg) {
+    try {
+        return std::make_unique<UnifiedMessageView<T>>(msg);
+    } catch (...) {
+        return nullptr;
+    }
+}
+
+class Tensor {
+};
+
+void test() {
+    // Direct
+    UnifiedMessage msg = Tensor{};
+
+    Tensor* t_v = as<Tensor>(msg);
+
+    // Shared
+    auto shared_msg = std::make_shared<UnifiedMessage>(Tensor{});
+
+    auto msg_view = asView<Tensor>(shared_msg);
+
+    Tensor* t = msg_view->get();
+}
+
+
 #include <type_traits>
 
 // https://www.youtube.com/watch?v=NH1Tta7purM&t=2694s&ab_channel=CppCon
@@ -39,6 +104,7 @@ struct OrderSenderB {
 
 struct IOrderManager {
     virtual void MainLoop() = 0;
+
     virtual ~IOrderManager() = default;
 };
 
