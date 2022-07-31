40d1993eb8293e91c271072ddcf930a87ec20062
Igor Lugansky <ilugansky@arrival.com>
2022-07-31 15:07:36 +0300

    save

--- projects/eff_14/summary.cpp
+++ projects/eff_14/summary.cpp
@@ -1,18 +1,32 @@
 
 #include <vector>
 
+#include "test_type.hpp"
+
 using namespace std;
 
 template<typename Container, typename Index>
-auto authAndAccess(Container& c, Index i) // -> decltype(c[i])
+// auto 
+decltype(auto)
+authAndAccess(
+	//Container& c
+	Container&& c
+, Index i) // -> decltype(c[i])
 {
   //authenticateUser();
-  return c[i];
+  return std::forward<Container>(c)[i];
 }
+
+vector<int> makeVec() {
+	return {1, 5, 6};
+}
+
 
 int main() {
 
 	vector<int> v{1, 5, 6};
 	authAndAccess(v, 0) = 1;
+
+	auto s = authAndAccess(makeVec(), 0);
 	return 0;
 }
---
+++ projects/eff_14/test_type.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <atomic>
+
+template <typename PayloadType>
+struct OptionalPayload {
+    PayloadType payload_;
+};
+
+template <>
+struct OptionalPayload<void> {};
+
+template <typename PayloadType = void>
+struct Checker {
+    Checker() { ctor_cnt++; }
+    Checker(const Checker&) noexcept { copy_ctor_cnt++; }
+    Checker(Checker&& that) noexcept {
+        that.was_moved_ = true;
+        move_ctor_cnt++;
+    }
+
+    Checker& operator=(Checker&& that) noexcept {
+        that.was_moved_ = true;
+        move_ctor_cnt++;
+        return *this;
+    }
+
+    ~Checker() {
+        if (was_moved_) {
+            move_dtor_cnt++;
+        } else {
+            dtor_cnt++;
+        }
+    }
+    void* operator new(size_t size) {
+        void* p = ::operator new(size);
+        new_cnt++;
+        return p;
+    }
+    void* operator new(size_t, void* place) { return place; }
+
+    void operator delete(void* p) {
+        delete_cnt++;
+        ::operator delete(static_cast<Checker*>(p));
+    }
+
+    static void flush() {
+        new_cnt = 0;
+        delete_cnt = 0;
+        ctor_cnt = 0;
+        dtor_cnt = 0;
+        copy_ctor_cnt = 0;
+        move_ctor_cnt = 0;
+        move_dtor_cnt = 0;
+    }
+    static volatile uint32_t new_cnt;
+    static volatile uint32_t delete_cnt;
+    static volatile uint32_t ctor_cnt;
+    static volatile uint32_t copy_ctor_cnt;
+    static volatile uint32_t move_ctor_cnt;
+    static volatile uint32_t dtor_cnt;
+    static volatile uint32_t move_dtor_cnt;
+
+    bool was_moved_ = false;
+
+    OptionalPayload<PayloadType> payload_;
+};
+
+template <typename PayloadType>
+volatile uint32_t Checker<PayloadType>::new_cnt = 0;
+template <typename PayloadType>
+volatile uint32_t Checker<PayloadType>::delete_cnt = 0;
+template <typename PayloadType>
+volatile uint32_t Checker<PayloadType>::ctor_cnt = 0;
+template <typename PayloadType>
+volatile uint32_t Checker<PayloadType>::copy_ctor_cnt = 0;
+template <typename PayloadType>
+volatile uint32_t Checker<PayloadType>::move_ctor_cnt = 0;
+template <typename PayloadType>
+volatile uint32_t Checker<PayloadType>::dtor_cnt = 0;
+template <typename PayloadType>
+volatile uint32_t Checker<PayloadType>::move_dtor_cnt = 0;
