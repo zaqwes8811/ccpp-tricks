9eac02ffd348a53ace5c81c343582f4ec28c54c1
Igor Lugansky <ilugansky@arrival.com>
2022-09-18 14:22:50 +0300

    save

--- edu/fastware/yuri_minaev/hypercritical_code_review_test.cpp
+++ edu/fastware/yuri_minaev/hypercritical_code_review_test.cpp
@@ -2,3 +2,12 @@
 // Created by zaqwes on 18.09.2022.
 //
 
+// "Aut'o'matic
+
+#include <vector>
+
+void foo(const std::vector<int>& vec) {
+    for(auto i = 0; i < vec.size(); ++i) {  // int i. Bad for x64
+
+    }
+}
