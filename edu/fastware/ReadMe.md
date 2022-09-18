faae28c20f439533747306a9dd9d7972552b0f9c
Igor Lugansky <ilugansky@arrival.com>
2022-09-18 22:01:59 +0300

    perf

--- edu/fastware/ReadMe.md
+++ edu/fastware/ReadMe.md
@@ -1,4 +1,29 @@
 
+Chandler Capur:
+- llvm
+  CppCon 2017: Chandler Carruth “Going Nowhere Faster”
+  CppCon 2015: Chandler Carruth "Tuning C++: Benchmarks, and CPUs, and Compilers! Oh My!"
+
+Perf help:
+```shell
+
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+
+perf_4.9 stat ./projects/high_perf/hp_benchmark
+
+perf_4.9 record -g ./projects/high_perf/hp_benchmark
+
+perf_4.9 report -g 'graph,0.5,caller'
+
+cat /proc/sys/kernel/perf_event_paranoid
+sudo sh -c 'echo -1 >/proc/sys/kernel/perf_event_paranoid'
+sudo sh -c 'echo 0 > /proc/sys/kernel/kptr_restrict'
+
+```
+
+
+################################################################
+
 Alexandrescu:
 - [code::dive conference 2015 - Andrei Alexandrescu - Writing Fast Code I](https://www.youtube.com/watch?v=vrfYLlR8X8k&ab_channel=NOKIATechnologyCenterWroc%C5%82aw)
 
@@ -15,5 +40,3 @@ Alan Talbot:
 Ben Deane:
 - [CppCon 2019: Ben Deane “Everyday Efficiency: In-Place Construction (Back to Basics?)”](https://www.youtube.com/watch?v=oTMSgI1XjF8&t=14s&ab_channel=CppCon)
 
-Chandler Capur:
-- llvm
--- projects/high_perf/zigzag_benchmark.cpp
+++ projects/high_perf/zigzag_benchmark.cpp
@@ -25,32 +25,33 @@ std::string gen_random(const int len) {
 // Define another benchmark
 static const int g_size = 256;
 static const int g_num_row = 2;
-static void BM_base(benchmark::State& state) {
-    std::string x = gen_random(g_size);
+static void BM_ZigZag(benchmark::State& state) {
+    std::string x = gen_random(state.range(0));
     for (auto _ : state) {
         auto result = leetcode::convert_base(x, g_num_row);
         benchmark::DoNotOptimize(result);
     }
 }
 
-BENCHMARK(BM_base);
 
-static void BM_base_best(benchmark::State& state) {
-    std::string x = gen_random(g_size);
+BENCHMARK(BM_ZigZag)->Arg(8)->Arg(16)->Arg(32)->Arg(64)->Arg(128)->Arg(g_size)->Arg(512);
+
+static void BM_ZigZag_best(benchmark::State& state) {
+    std::string x = gen_random(state.range(0));
     for (auto _ : state) {
         auto result = leetcode::convert_best(x, g_num_row);
         benchmark::DoNotOptimize(result);
     }
 }
 
-BENCHMARK(BM_base_best);
+BENCHMARK(BM_ZigZag_best)->Arg(8)->Arg(16)->Arg(32)->Arg(64)->Arg(128)->Arg(g_size)->Arg(512);
 
-static void BM_base_ref(benchmark::State& state) {
-    std::string x = gen_random(g_size);
+static void BM_ZigZag_ref(benchmark::State& state) {
+    std::string x = gen_random(state.range(0));
     for (auto _ : state) {
         auto result = leetcode::convert_ref(x, g_num_row);
         benchmark::DoNotOptimize(result);
     }
 }
 
-BENCHMARK(BM_base_ref);
+//BENCHMARK(BM_ZigZag_ref)->Arg(8)->Arg(16)->Arg(32)->Arg(64)->Arg(128)->Arg(g_size)->Arg(512);;
