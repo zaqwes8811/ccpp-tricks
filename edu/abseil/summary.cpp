adf303686b3321386862744583aa607e1a5c555b
Igor Lugansky <ilugansky@arrival.com>
2022-10-08 20:35:14 +0400

    ave

--- edu/abseil/summary.cpp
+++ edu/abseil/summary.cpp
@@ -4,3 +4,23 @@
 
 //
 // https://abseil.io/tips/1
+#include <string_view>
+#include <string>
+
+void TakesCharStar(const char* s) {}
+
+void TakesString(const std::string& s){}
+
+void TakesStringView(std::string_view s){  // by value
+    const auto str_copy = std::string(s);
+}
+
+void AlreadyHasString(const std::string& s) {
+//    TakesCharStar(s.c_str());
+    TakesStringView(s);
+}
+
+void AlreadyHasCharStar(const char* s) {
+//    TakesString(s);
+    TakesStringView(s);
+}
